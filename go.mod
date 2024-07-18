9eb9762fc5bc49b42e03b332262af08820d7d502
hezhizhen <hezhizhen.yi@gmail.com>
2024-07-18 15:59:09 +0800

    example: add sort language versions

---
+++ go.mod
@@ -0,0 +1,5 @@
+module examples
+
+go 1.22
+
+require github.com/Masterminds/semver/v3 v3.2.1
---
+++ sort_language_versions/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/Masterminds/semver/v3"
+)
+
+type Language struct {
+	Language string
+	Version  string
+}
+
+var items = []Language{
+	{"cpp", "2.7"}, {"cpp", "2020"},
+	{"golang", "1.21"}, {"golang", "1.20"}, {"golang", "1.19"}, {"golang", "1.12.13"}, {"golang", "1.17"}, {"golang", "1.8"},
+	{"lua", "5"},
+}
+
+func main() {
+	m := make(map[string][]string)
+	for _, one := range items {
+		m[one.Language] = append(m[one.Language], one.Version)
+	}
+	for l, vs := range m {
+		fmt.Println(l)
+		fmt.Printf("original:\t%s\n", m[l])
+		m[l] = sortVersions(vs)
+		fmt.Printf("sorted:\t\t%s\n", m[l])
+	}
+}
+
+func sortVersions(vs []string) []string {
+	var versions []*semver.Version
+	for _, v := range vs {
+		versions = append(versions, semver.MustParse(v))
+	}
+	// newer version comes first
+	sort.Sort(sort.Reverse(semver.Collection(versions)))
+	var ret []string
+	for _, v := range versions {
+		ret = append(ret, v.Original())
+	}
+	return ret
+}
