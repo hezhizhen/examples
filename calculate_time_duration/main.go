f713b0e4a50c132b16b13ce40fbf92aee1e5268b
Zhizhen He <hezhizhen.yi@gmail.com>
2024-10-16 15:13:48 +0800

    example: add more units for time duration

--- calculate_time_duration/main.go
+++ calculate_time_duration/main.go
@@ -9,7 +9,10 @@ func main() {
 	start := time.Now()
 	end := start.Add(time.Minute)
 
-	fmt.Println(end.Sub(start))                      // 1m0s
-	fmt.Println(end.Unix() - start.Unix())           // 60    unit: second
-	fmt.Println(end.UnixMilli() - start.UnixMilli()) // 60000 unit: millisecond
+	fmt.Println(end.Sub(start)) // 1m0s
+
+	fmt.Println(end.Unix() - start.Unix())           // 60          seconds
+	fmt.Println(end.UnixMilli() - start.UnixMilli()) // 60000       milliseconds
+	fmt.Println(end.UnixMicro() - start.UnixMicro()) // 60000000    microseconds
+	fmt.Println(end.UnixNano() - start.UnixNano())   // 60000000000 nanoseconds
 }
